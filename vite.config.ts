a50a571819070e3d6e9a043cb1827f68776c347c
BCR <154340634+YassineBCR@users.noreply.github.com>
2026-03-31 21:32:50 +0200

    ezaa

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    entries: ['index.html'],
-    exclude: ['lucide-react', 'html5-qrcode'],
-  },
-  build: {
-    commonjsOptions: {
-      // Évite que Rollup plante sur des modules CJS complexes
-      transformMixedEsModules: true,
-    },
+    exclude: ['lucide-react'],
   },
 });
